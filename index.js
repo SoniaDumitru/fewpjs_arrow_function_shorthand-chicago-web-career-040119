f3101520bdd11aabf215103a22fb1d55193b197d
Sonia Dumitru <soni.dumitru@gmail.com>
2019-05-21 23:17:42 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,10 @@
+//function expression
+let divide = () => {return 2000/100};
+//arrow functions square & add
+//let divide = () => 2000/100;
+
+let square = num => {return num*num};
+//let square = num => num * num;
+
+let add = (num1, num2) => {return num1 + num2};
+// let add = (num1, num2) => num1 + num2;
